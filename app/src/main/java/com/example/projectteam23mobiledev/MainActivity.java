fdedf2ec609caa38316eca3af789240ed6a6af6c
Chayank11 <chayank.tm@gmail.com>
2022-04-13 15:17:18 -0400

    bottom navigation bar

--- app/src/main/java/com/example/projectteam23mobiledev/MainActivity.java
+++ app/src/main/java/com/example/projectteam23mobiledev/MainActivity.java
@@ -1,18 +1,30 @@
 package com.example.projectteam23mobiledev;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
-    private TextView usr;
+//    private TextView usr;
+    private BottomNavigationView bottomNavigationView;
+
+    private RunFragment runFragment = new RunFragment();
+    private ProfileFragment profileFragment = new ProfileFragment();
+    private ChallengeFragment challengeFragment = new ChallengeFragment();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +32,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
-        usr = (TextView) findViewById(R.id.usr);
+//        usr = (TextView) findViewById(R.id.usr);
 
         if (mAuth.getCurrentUser() != null) {
             String em = mAuth.getCurrentUser().getEmail();
-            usr.setText("Hi "+em.substring(0, em.indexOf('@'))+ "!");
+//            usr.setText("Hi "+em.substring(0, em.indexOf('@'))+ "!");
         }
+
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, runFragment).commit();
+
+//        BadgeDrawable badgeDrawable = bottomNavigationView.getOrCreateBadge(R.id.profile);
+//        badgeDrawable.setVisible(true);
+//        badgeDrawable.setNumber(8);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.run:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, runFragment).commit();
+                        return true;
+                    case R.id.profile:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, profileFragment).commit();
+                        return true;
+                    case R.id.challenge:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, challengeFragment).commit();
+                        return true;
+                }
+
+                return false;
+            }
+        });
     }
 
     @Override
