e38389fdac550f3cf9612219ff54545a3e17e6fd
Chayank11 <chayank.tm@gmail.com>
2022-04-19 22:54:32 -0400

    fixed bug for start run fragment

--- app/src/main/java/com/example/projectteam23mobiledev/StartRunActivity.java
+++ app/src/main/java/com/example/projectteam23mobiledev/StartRunActivity.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 public class StartRunActivity extends Fragment implements View.OnClickListener {
@@ -25,10 +26,11 @@ public class StartRunActivity extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View view) {
         Fragment fragment = new RunFragment();
-        FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        transaction.replace(R.id.start_run_view, fragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 
     @Override
